08ce9e6d6c5fb502fc242ab8ecbae81209e301c5
ozakione <29860391+OzakIOne@users.noreply.github.com>
2023-09-29 18:34:14 +0200

    fix: path error

--- tests/siteScreenshots.spec.ts
+++ tests/siteScreenshots.spec.ts
@@ -3,8 +3,8 @@ import { argosScreenshot } from "@argos-ci/playwright";
 import * as fs from "fs";
 import * as cheerio from "cheerio";
 
-const siteUrl = process.env.SITE_URL ?? "https://metro-git-ozaki-update-deps-lorbersebastien.vercel.app/metro";
-const isProd = siteUrl === "https://facebook.github.io/metro";
+const siteUrl = process.env.SITE_URL ?? "https://metro-git-ozaki-update-deps-lorbersebastien.vercel.app";
+const isProd = siteUrl === "https://facebook.github.io";
 
 console.log({ siteUrl, isProd });
 
